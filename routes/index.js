eaec7d6bdcfe019701757096a6173503ef58643f
William-Noble <william.noble1@ontariotechu.net>
2024-11-05 13:13:44 -0500

    introduced a navbar into index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
-});
-
-/* GET contact page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Information' });
-});
-
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
@@ -19,6 +9,16 @@ router.get('/home', function(req, res, next) {
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'My Projects' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Information' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
 });
 
 module.exports = router;
